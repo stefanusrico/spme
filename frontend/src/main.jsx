33d39ae1a48251dff2aee4f605702c54267390c8
stefanusrico <stefanus.rico70@gmail.com>
2025-03-13 00:08:46 +0700

    update: remove strict mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import ReactDOM from "react-dom/client"
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
 import { handleLogout } from "./components/Auth/auth.action"
@@ -19,11 +19,20 @@ import Jadwal from "./pages/Jadwal"
 import JsonGenerator from "./pages/JsonGenerator"
 import LoginPage from "./pages/login"
 import Notifications from "./pages/Notifications"
+import PengisianMatrikLed from "./pages/PengisianMatriksLed"
 import Projects from "./pages/Projects"
 import RegisterPage from "./pages/register"
 import UserManagement from "./pages/userManagement"
-import PengisianMatrikLed from "./pages/PengisianMatriksLed"
 import { isTokenExpired } from "./utils/axiosConfig"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 const CHECK_INTERVAL = 1000
 
@@ -126,9 +135,7 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </QueryClientProvider>
 )
-
-//auth
