2b860359b629d37a7a6aa8e7183f5bed4e612487
stefanusrico <stefanus.rico70@gmail.com>
2025-06-30 15:29:14 +0700

    remove: basename siaps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,31 +8,9 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    {
-      name: "redirect-middleware",
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          const url = req.originalUrl || req.url
-
-          if (url === "/" || url === "/siaps") {
-            res.writeHead(302, { Location: "/siaps/" })
-            res.end()
-            return
-          }
-
-          next()
-        })
-      },
-    },
-  ],
+  plugins: [react(), tailwindcss()],
   define: {
     global: "window",
-  },
-  optimizeDeps: {
-    include: ["jwt-decode", "mathjs"],
   },
   resolve: {
     alias: {
@@ -65,10 +43,7 @@ export default defineConfig({
       port: 4000,
     },
     allowedHosts: ["polban-space.cloudias79.com"],
-    historyApiFallback: {
-      disableDotRule: true,
-      index: "/siaps/",
-    },
+    historyApiFallback: true,
   },
   preview: {
     watch: {
