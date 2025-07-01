eee9bf44a2fd68df6032504b70f6bcef49f74b70
stefanusrico <stefanus.rico70@gmail.com>
2025-07-02 00:11:54 +0700

    update: change url to static image

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -7,7 +7,7 @@ const LoginPage = () => {
     <div className="flex flex-col md:flex-row min-h-screen bg-gray-100 overflow-hidden">
       <div className="w-full md:w-3/4 h-screen relative">
         <img
-          src="https://ppb.polban.ac.id/storage/theme-images/01JHPMYXBED430J4QRHQD4336Q.jpeg"
+          src="pendopo.jpeg"
           alt="Pendopo POLBAN"
           className="absolute inset-0 w-full h-full object-cover"
         />
